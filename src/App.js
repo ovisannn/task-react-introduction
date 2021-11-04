ca63c8a427c38c32fb3f647c0d923fd4cd24dad7
ovisannn <ovi.sanjaya11@gmail.com>
2021-11-04 09:56:49 +0700

    task 12 initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
-import Home from './Home'
-import Contact from './Contact-us'
-
-
+import Home from "./pages/components/Home";
 
 function App() {
   return (
-    <div>
+    <>
       <Home/>
-      <Contact/>
-    </div>
+    </>
   );
 }
 
